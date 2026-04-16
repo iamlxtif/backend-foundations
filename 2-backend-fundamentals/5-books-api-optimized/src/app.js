4cd52dbfd562a8b785e1b859fe602149bc30a627
Abdelatif Guendouz <abdelatifguendouz72@gmail.com>
2026-04-16 16:04:59 +0100

    feat: add morgan request logging

--- 2-backend-fundamentals/5-books-api-optimized/src/app.js
+++ 2-backend-fundamentals/5-books-api-optimized/src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { config } from 'dotenv'
 import { errorHandler } from './middleware/errorHandler'
+import morgan from 'morgan'
 
 config()
 
 const app = express()
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.get('/health',(req,res) => {
     res.json({status: 'ok', timestamp: new Date().toISOString()})
