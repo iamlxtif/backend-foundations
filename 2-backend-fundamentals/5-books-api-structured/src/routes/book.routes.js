fd66421824f9ad2fad384ca871a0b33ccfc60b59
Abdelatif Guendouz <abdelatifguendouz72@gmail.com>
2026-04-17 12:13:36 +0100

    fix: re-install express package

--- 2-backend-fundamentals/5-books-api-structured/src/routes/book.routes.js
+++ 2-backend-fundamentals/5-books-api-structured/src/routes/book.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { createBook, deleteBook, getbook, getbooks, updateBook } from '../controllers/books.controller'
+import { createBook, deleteBook, getbook, getbooks, updateBook } from '../controllers/books.controller.js'
 
 const router = Router()
 
